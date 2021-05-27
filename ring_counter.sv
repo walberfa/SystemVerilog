0155b6d6db6dcf4f61d41283ce23e53748181636
Walber Florêncio <59696629+walberfa@users.noreply.github.com>
2021-05-27 18:15:46 -0300

    Update ring_counter.sv

--- ring_counter.sv
+++ ring_counter.sv
@@ -11,14 +11,17 @@ module ring_counter
   always_ff @(posedge clk, posedge reset) 
     
     begin
+	//register
       if (reset)
       	r_reg = 4'b1000;
       else if (clk)
         r_reg = r_next;
     end
-      
+	
+      //next_state logic
   	assign r_next = r_reg[0] & r_reg[WIDTH-1:1];
-  
+     
+      //output logic
   	assign q = r_reg;
   
 endmodule
